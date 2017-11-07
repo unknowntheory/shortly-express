a0c2aab5c3537cc74b8fad9f6e9fc2dc974f91d4
Hack Reactor Student <student@hackreactor.com>
2017-11-07 12:02:39 -0800

    handling cookie assignment

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,18 +2,36 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
-  // console.log(, "THIS IS THE REQ IN AUTH");
-  // console.log('DIS NEXT', next)
-  if (req.headers.cookie) {
-    // console.log('there are cookies')
-  } else {
-    models.Sessions.create()
+  if (req.cookies && Object.keys(req.cookies).length > 0) {
+    // console.log('heloooo cookies', req.cookies);
+    models.Sessions.get({hash: req.cookies.shortlyid})
       .then(session => {
-        return models.Sessions.get({id: session.insertId})
-          .then(session =>{
+        req.session = session;
+        // console.log('SESSION---', session);
+        return models.Users.get({id: session.id});
+      })
+      .then(user => {
+        // console.log('USERERR', user);
+        next();
+      })
+      .catch(err => {
+        // clear and reassing new cookie
+        req.cookies = {};
+        module.exports.createSession(req, res, next);
+      });
+  } 
+  if (Object.keys(req.cookies).length === 0) {
+    models.Sessions.create()
+      .then(results => {
+        return models.Sessions.get({id: results.insertId})
+          .then(session => {
             req.session = session;
-            //console.log(res, "RESPONSEEEEEE")
+            res.cookies.shortlyid = {
+              'value': session.hash
+            };
             next();
+            // session.username = something
+            // session.userId = something
           });
       });
   }
